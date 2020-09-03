04c290f52e73cc6215d61e41a0cb87dd5a5ddbf0
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-02 23:33:53 -0300

    Update kubeflow.sh

--- kubeflow.sh
+++ kubeflow.sh
@@ -12,3 +12,4 @@ echo " > Acessar o UI do Kubeflow"
 export INGRESS_HOST=$(curl -s checkip.amazonaws.com)
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
 echo "Acessar: http://$INGRESS_HOST:$INGRESS_PORT"
+echo "Executar novamente: $ kubectl get pod --all-namespaces"
