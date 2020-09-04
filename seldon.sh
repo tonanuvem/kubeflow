97cbc6b48709dc7500526107a77f11520a698901
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-04 17:28:30 -0300

    Update seldon.sh

--- seldon.sh
+++ seldon.sh
@@ -6,8 +6,10 @@ echo "kubectl expose deployment $(kubectl get deploy -n seldon -o jsonpath='{.it
 #kubectl create service nodeport modelo --node-port 31000 --tcp=8000:8000 -n seldon
 export HOST=$(curl -s checkip.amazonaws.com)
 export SVC_PORT=$(kubectl -n seldon get service modelo -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
-echo "Acessar: http://$HOST:$SVC_PORT"
-#curl -X POST http://$HOST:$SVC_PORT/seldon/seldon/iris-model/api/v1.0/predictions \
+echo "Acessando: http://$HOST:$SVC_PORT"
+# TODO : ajeitar o ingress para ficar o caminho completo e funcionar:
+# curl http://$HOST:$SVC_PORT/seldon/seldon/iris-model/api/v1.0/doc/
+# curl -X POST http://$HOST:$SVC_PORT/seldon/seldon/iris-model/api/v1.0/predictions -H 'Content-Type: application/json' -d '{ "data": { "ndarray": [[1,2,3,4]] } }'
 echo "curl -X POST http://$HOST:$SVC_PORT/api/v1.0/predictions -H 'Content-Type: application/json' -d '{ "data": { "ndarray": [[1,2,3,4]] } }'"
 print
 curl -X POST http://$HOST:$SVC_PORT/api/v1.0/predictions \
