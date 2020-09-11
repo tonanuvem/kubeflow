5bcf3960a04ce517c003213f9583093bf77e43d9
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-09-11 13:41:24 -0300

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,4 +1,4 @@
 export INGRESS_HOST=$(curl -s checkip.amazonaws.com)
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
-echo "	Acessar o UI do Kubeflow:
+echo "Acessar o UI do Kubeflow:"
 echo $INGRESS_HOST:$INGRESS_PORT
